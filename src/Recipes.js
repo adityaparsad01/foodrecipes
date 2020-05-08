97e9200a1d2232ebb7a601ba0326bab4fa94d475
unknown <adityaparsad01@gmail.com>
2020-05-08 17:32:11 +0530

    color improvment

--- src/Recipes.js
+++ src/Recipes.js
@@ -4,7 +4,7 @@ import style from './recipe.module.css';
 const Recipe = ({title,Calories,image,ingredients}) => {
     return(
         <div className={style.recipe}>
-            <h1 key={title.id}>{title}</h1>
+            <h1 className={style.title} key={title.id}>{title}</h1>
             <ul className={style.card}>
                 {ingredients.map(ingredients => (
                     <li>{ingredients.text}</li>
